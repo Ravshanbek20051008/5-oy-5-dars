898eeb5f2432157b0249e990c2d65b00865050e1
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-07-11 14:34:16 +0500

    html tugadi

--- js/index.js
+++ js/index.js
@@ -11,18 +11,34 @@
 //     console.log(title);
 //   });
 
-const from = document.querySelector("#from");
-const name = document.querySelector("#name");
-const age = document.querySelector("#age");
-const nationality = document.querySelector("nationalitym");
-const description = document.querySelector("#description");
-const button = document.querySelector("#button");
+// const form = document.querySelector("#from");
+// const name = document.querySelector("#name");
+// const age = document.querySelector("#age");
+// const nationality = document.querySelector("nationalitym");
+// const description = document.querySelector("#description");
+// const button = document.querySelector("#button");
 
-from &&
-  from.addEventListener("sumbit", function (event) {
-    event.preventDefault();
-    console.Log(name.value);
-    console.Log(age.value);
-    console.Log(nationality.value);
-    console.Log(description.value);
-  });
+// from &&
+//   from.addEventListener("sumbit", function (event) {
+//     event.preventDefault();
+//     console.Log(name.value);
+//     console.Log(age.value);
+//     console.Log(nationality.value);
+//     console.Log(description.value);
+//   });
+
+//   vazifa
+const form = document.getElementById("#from");
+form.style.width = "1393px";
+form.style.display = "flex";
+form.style.flexDirection = "colum";
+form.style.marginLeft = "527px";
+form.style.backgroundColor = "#242424";
+
+const vj = document.getElementById("VJ");
+vj.style.width = "800px";
+vj.style.backgroundColor = "#3d3d3d";
+vj.style.paddingLeft = "297px";
+vj.style.paddingRight = "296px";
+vj.style.borderRadius = "4px";
+vj.style.border = "none";
