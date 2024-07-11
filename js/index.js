74b6a83844a0bb023ef5760d416d60fd18fdc092
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-07-11 17:13:36 +0500

    wrapper qoldi

--- js/index.js
+++ js/index.js
@@ -1,135 +1,4 @@
-// // const blok = document.querySelector("#blok");
-// // const button = document.querySelector("#btn");
-// // button &&
-// //   button.addEventListener("click", function (event) {
-// //     event.preventDefault();
-// //     const title = document.createElement("p");
-// //     title.innerHTML = "salom dunyo";
-// //     title.setAttribute("class", "id");
-// //     title.setAttribute("id", "sarlavha");
-// //     blok.appendChild(title);
-// //     console.log(title);
-// //   });
-
-// // const form = document.querySelector("#from");
-// // const name = document.querySelector("#name");
-// // const age = document.querySelector("#age");
-// // const nationality = document.querySelector("nationalitym");
-// // const description = document.querySelector("#description");
-// // const button = document.querySelector("#button");
-
-// // from &&
-// //   from.addEventListener("sumbit", function (event) {
-// //     event.preventDefault();
-// //     console.Log(name.value);
-// //     console.Log(age.value);
-// //     console.Log(nationality.value);
-// //     console.Log(description.value);
-// //   });
-
-// //   vazifa
-
-// const form = document.querySelector("#from");
-// const title = document.querySelector("#title");
-// const title_p = document.querySelector("#title_p");
-// const name = document.querySelector("#name");
-// const age = document.querySelector("#age");
-// const MobileNumber = document.querySelector("#MobileNumber");
-// const EmailAddress = document.querySelector("#EmailAddress");
-// const Password = document.querySelector("#Password");
-// const ConfirmPassword = document.querySelector("#ConfirmPassword");
-// const title_h3 = document.querySelector("#title_h3");
-// const button = document.querySelector("#button");
-
-// function validate() {
-//   /* bu berilgan vj rathod lar hindcha ekan yosh
-//  bosa kerak deb oylab olib ketordim */
-//   if (name.value.length > 3) {
-//     alert("ism 3 ta belgidan kam bolmasin");
-//     name.focus();
-//     name.style.outLineColor = "red";
-//     return false;
-//   }
-//   if (age.value < 0 || age.value > 150) {
-//     alert("yosh bunday qiymat qabul qilmaydi");
-//     age.focus();
-//     age.style.outLineColor = "red";
-//     return false;
-//   }
-//   if (MobileNumber.value < 0 || MobileNumber.value > 150) {
-//     alert("yosh bunday qiymat qabul qilmaydi");
-//     MobileNumber.focus();
-//     MobileNumber.style.outLineColor = "red";
-//     return false;
-//   }
-//   if (EmailAddress.value.length < 5) {
-//     alert("yosh bunday qiymat qabul qilmaydi");
-//     EmailAddress.focus();
-//     EmailAddress.style.outLineColor = "red";
-//     return false;
-//   }
-//   if (Password.value < 0 || Password.value > 15) {
-//     alert("yosh bunday qiymat qabul qilmaydi");
-//     Password.focus();
-//     Password.style.outLineColor = "red";
-//     return false;
-//   }
-//   if (ConfirmPassword.value < 0 || ConfirmPassword.value > 15) {
-//     alert("yosh bunday qiymat qabul qilmaydi");
-//     ConfirmPassword.focus();
-//     ConfirmPassword.style.outLineColor = "red";
-//     return false;
-//   }
-//   return true;
-// }
-// function createCard(user) {
-//   let card = document.createElement("div");
-//   card.setAttribute("card");
-//   const nameElement = document.createElement("h3");
-//   nameElement.innerHTML = user.name;
-//   card.appendChild(nameElement);
-//   const ageElement = document.createElement("h3");
-//   ageElement.innerHTML = user.age;
-//   card.appendChild(ageElement);
-//   const MobileNumberElement = document.createElement("h3");
-//   MobileNumberElement.innerHTML = user.MobileNumber;
-//   card.appendChild(MobileNumberElement);
-//   const EmailAddressElement = document.createElement("h3");
-//   EmailAddressElement.innerHTML = user.EmailAddress;
-//   card.appendChild(EmailAddressElement);
-//   const PasswordElement = document.createElement("h3");
-//   PasswordElement.innerHTML = user.Password;
-//   card.appendChild(PasswordElement);
-//   const ConfirmPasswordelement = document.createElement("h3");
-//   ConfirmPasswordelement.innerHTML = user.ConfirmPassword;
-//   card.appendChild(ConfirmPasswordelement);
-//   return card;
-// }
-// button &&
-//   button.addEventListener("sumbit", function (event) {
-//     event.preventDefault();
-//     const isValid = validate();
-//     if (!isValid) {
-//       return;
-//     }
-
-//     let user = {
-//       name: name.value,
-//       age: age.value,
-//       MobileNumber: MobileNumber.value,
-//       EmailAddress: EmailAddress.value,
-//       Password: Password.value,
-//       ConfirmPassword: ConfirmPassword.value,
-//     };
-//     let card = createCard(user);
-//     wrapper.appenChild(card);
-//     console.log("validatsiyadan o'ttingiz");
-//   });
-
-// // button &&
-// //   button.addEventListener("sumbit", function (event) {
-// //     event.preventDefault();
-// //   });
+// vazifa
 const form = document.querySelector("#form");
 const title = document.querySelector("#title");
 const title_p = document.querySelector("#title_p");
@@ -213,6 +82,20 @@ button.addEventListener("click", function (event) {
   if (!isValid) {
     return;
   }
+  function resetForm() {
+    name.value = "";
+    age.value = "";
+    MobileNumber.value = "";
+    EmailAddress.value = "";
+    Password.value = "";
+    ConfirmPassword.value = "";
+    name.style.outline = "";
+    age.style.outline = "";
+    MobileNumber.style.outline = "";
+    EmailAddress.style.outline = "";
+    Password.style.outline = "";
+    ConfirmPassword.style.outline = "";
+  }
 
   let user = {
     name: name.value,
@@ -224,5 +107,5 @@ button.addEventListener("click", function (event) {
   };
   let card = createCard(user);
   wrapper.appendChild(card);
-  button.reset();
+  resetForm();
 });
